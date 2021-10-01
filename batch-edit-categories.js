5ae6b61906f1e30120f0c65031b18d2758244259
sadaszewski <s.adaszewski@gmail.com>
2021-10-01 17:42:17 +0200

    Update batch-edit-categories.js

--- batch-edit-categories.js
+++ batch-edit-categories.js
@@ -9,57 +9,63 @@ const db = require(path.resolve(NODEBB_PATH, 'src/db'));
 const prestart = require(path.resolve(NODEBB_PATH, 'src/prestart'));
 const categories = require(path.resolve(NODEBB_PATH, 'src/categories'));
 
-nconf.env({
+async function main() {
+	nconf.env({
 	separator: '__',
-});
+	});
 
-prestart.setupWinston();
+	prestart.setupWinston();
 
-const	configFile = path.resolve(NODEBB_PATH, nconf.get('config') || 'config.json');
+	const	configFile = path.resolve(NODEBB_PATH, nconf.get('config') || 'config.json');
 
-prestart.loadConfig(configFile);
+	prestart.loadConfig(configFile);
 
-await db.init();
+	await db.init();
 
-async function getCid(name) {
-  let cids = await db.getSortedSetScan({
-    key: 'categories:name',
-    match: name
-  });
-  cids = cids.map(data => parseInt(data.split(':').pop(), 10));
-  if (!cids.length)
-    throw Error("Not found");
-  return cids[0];
+	async function getCid(name) {
+	  let cids = await db.getSortedSetScan({
+	    key: 'categories:name',
+	    match: name
+	  });
+	  cids = cids.map(data => parseInt(data.split(':').pop(), 10));
+	  if (!cids.length)
+	    throw Error("Not found");
+	  return cids[0];
+	}
+
+	async function getAllCids() {
+	  let cids = await db.getSortedSetScan({
+	    key: 'categories:name'
+	  });
+	  cids = cids.map(data => parseInt(data.split(':').pop(), 10));
+	  return cids;
+	}
+
+	program
+	  .name('./batch-edit-categories')
+	  .description('Batch edit categories')
+	  .option('--root-category', 'Root category', null)
+	  .option('--properties-file-name <filename>', 'JSON properties file name', 'category-properties.json')
+	  .action(async () => {
+	    const opts = program.opts();
+	    const { rootCategory, propertiesFileName } = opts;
+
+	    let cats = [];
+
+	    if (rootCategory) {
+	      const rootCid = await getCid(rootCategory);
+	      cats.push(rootCid);
+	      cats = cats.concat(await categories.getChildrenCids(rootCid));
+	    } else {
+	      cats = cats.concat(await getAllCids());
+	    }
+
+	    console.log('cats: ' + cats);
+	  });
+
+	program.parse();
 }
 
-async function getAllCids() {
-  let cids = await db.getSortedSetScan({
-    key: 'categories:name'
-  });
-  cids = cids.map(data => parseInt(data.split(':').pop(), 10));
-  return cids;
-}
+main().then(() => { console.log('ok'); }).catch(() => { console.log('catch'); });
 
-program
-  .name('./batch-edit-categories')
-  .description('Batch edit categories')
-  .option('--root-category', 'Root category', null)
-  .option('--properties-file-name <filename>', 'JSON properties file name', 'category-properties.json')
-  .action(async () => {
-    const opts = program.opts();
-    const { rootCategory, propertiesFileName } = opts;
-  
-    let cats = [];
-  
-    if (rootCategory) {
-      const rootCid = await getCid(rootCategory);
-      cats.push(rootCid);
-      cats = cats.concat(await categories.getChildrenCids(rootCid));
-    } else {
-      cats = cats.concat(await getAllCids());
-    }
-  
-    console.log('cats: ' + cats);
-  });
 
-program.parse();
