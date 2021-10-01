549bfb6bbd8f4f19b518dafb580fd61dd2f00b3f
sadaszewski <s.adaszewski@gmail.com>
2021-10-01 17:36:03 +0200

    Create batch-edit-categories.js

---
+++ batch-edit-categories.js
@@ -0,0 +1,65 @@
+const NODEBB_PATH = process.env['NODEBB_PATH'];
+
+const _ = require('lodash');
+const { program } = require('commander');
+const path = require('path');
+const fs = require('fs');
+const nconf = require('nconf');
+const db = require(path.resolve(NODEBB_PATH, 'src/db'));
+const prestart = require(path.resolve(NODEBB_PATH, 'src/prestart'));
+const categories = require(path.resolve(NODEBB_PATH, 'src/categories'));
+
+nconf.env({
+	separator: '__',
+});
+
+prestart.setupWinston();
+
+const	configFile = path.resolve(NODEBB_PATH, nconf.get('config') || 'config.json');
+
+prestart.loadConfig(configFile);
+
+await db.init();
+
+async function getCid(name) {
+  let cids = await db.getSortedSetScan({
+    key: 'categories:name',
+    match: name
+  });
+  cids = cids.map(data => parseInt(data.split(':').pop(), 10));
+  if (!cids.length)
+    throw Error("Not found");
+  return cids[0];
+}
+
+async function getAllCids() {
+  let cids = await db.getSortedSetScan({
+    key: 'categories:name'
+  });
+  cids = cids.map(data => parseInt(data.split(':').pop(), 10));
+  return cids;
+}
+
+program
+	.name('./batch-edit-categories')
+	.description('Batch edit categories')
+	.option('--root-category', 'Root category', null)
+	.option('--properties-file-name <filename>', 'JSON properties file name', 'category-properties.json')
+  .action(async () => {
+    const opts = program.opts();
+    const { rootCategory, propertiesFileName } = opts;
+  
+    let cats = [];
+  
+    if (rootCategory) {
+      const rootCid = await getCid(rootCategory);
+      cats.push(rootCid);
+      cats.extend(await categories.getChildrenCids(rootCid));
+    } else {
+      cats.extend(await getAllCids());
+    }
+  
+    console.log('cats: ' + cats);
+  });
+
+program.parse();
